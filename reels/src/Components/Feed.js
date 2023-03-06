da2773e5b8cd11295e55b2baba04a6e7abaf9bf4
Mani <bhushan.mani72@gmail.com>
2023-03-06 06:04:32 +0000

    add feed js

--- reels/src/Components/Feed.js
+++ reels/src/Components/Feed.js
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
+import UploadFile from "./Uploadfile";
 
 function Feed() {
   const { logout } = useContext(AuthContext);
   return (
     <h1>
       Welcome to feed <button onClick={logout}>Log out</button>
+      <UploadFile />
     </h1>
   );
 }
